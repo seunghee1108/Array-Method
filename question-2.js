125279c2509cff55da1eb18e565feb9d35e7969b
승희 <bangseunghee1108@gmail.com>
2023-11-09 22:14:22 +0900

    문제 2번 완료

---
+++ question-2.js
@@ -0,0 +1,9 @@
+// 문제 2번 : 배열에서 마지막 요소를 제거하고 그 요소를 반환하는 메서드는 무엇인가요?
+
+const pokemon = ["이상해씨", "파이리", "꼬부기"]
+const removePokemon = pokemon.pop();
+
+console.log(pokemon);
+// [ '이상해씨', '파이리' ]
+console.log(removePokemon); 
+// 꼬부기
