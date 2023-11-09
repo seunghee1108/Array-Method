c98fc50f661c577b550d6107c0452a6862d05563
승희 <bangseunghee1108@gmail.com>
2023-11-09 23:33:36 +0900

    문제 8번 완료

---
+++ question-8.js
@@ -0,0 +1,9 @@
+// 문제 8번 : 매열의 모든 요소를 연결해 하나의 문자열을 만드는 메서는 무엇인가요?
+
+const pokemon = ["이상해씨", "파이리", "꼬부기"]
+
+const joinedString = pokemon.join(", ");
+
+console.log(joinedString);
+// 이상해씨, 파이리, 꼬부기
+
