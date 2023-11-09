839b7d13b1df375b336546a126d72d76c0260336
승희 <bangseunghee1108@gmail.com>
2023-11-09 22:12:07 +0900

    문제 1번 완료

---
+++ question-1.js
@@ -0,0 +1,9 @@
+// 문제 1번 : 배열의 끝에 하나 이상의 요소를 추가하고, 배열의 새 길이를 반환하는 메서드는 무엇인가요?
+
+const pokemon = ["이상해씨", "파이리", "꼬부기"]
+const newLength = pokemon.push();
+
+console.log(pokemon);
+// [ '이상해씨', '파이리', '꼬부기' ]
+console.log(newLength);
+// 3 (새로운 배열의 길이)
