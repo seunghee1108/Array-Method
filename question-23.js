9c1ca9f59bbfe6ab591e8d2097a34d1b3be27416
승희 <bangseunghee1108@gmail.com>
2023-11-10 07:08:27 +0900

    문제 23번 완료

---
+++ question-23.js
@@ -0,0 +1,14 @@
+// 문제 23번 : 먼저 주어진 함수를 배열의 각 요소에 매핑한 다음 결과를 새 배열로 평탄화하는 메서드는 무엇인가요?
+
+const numbers = [1, 2, 3, 4, 5];
+const doubledAndFlattened = numbers.flatMap(number => [number, number * 2]);
+
+console.log(doubledAndFlattened);  // [1, 2, 2, 4, 3, 6, 4, 8, 5, 10] (각 요소를 2배로 늘린 후 평탄화)
+
+
+const array = [1, 2, 3, 4, 5];
+const flatNestedSquares = array.flatMap(function(number) {
+  return [number * number];
+});
+
+console.log(flatNestedSquares);
